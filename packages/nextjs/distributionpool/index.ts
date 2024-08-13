d78e60863273f6b628ab3070330a5bfee43c2eb7
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-08-13 14:13:55 +0100

    update superfluid functions

---
+++ packages/nextjs/distributionpool/index.ts
@@ -0,0 +1,80 @@
+import { useAccount } from "wagmi";
+import { ethers } from "ethers";
+import Abi from "~~/contracts/abi.json"
+import { useState } from "react";
+
+const token = "0x671425ae1f272bc6f79bec3ed5c4b00e9c628240";
+
+// const { writeContractAsync } = useScaffoldWriteContract("GDAv1Forwarder")
+let provider: any;
+if (typeof window !== 'undefined' && window.ethereum) {
+    provider = new ethers.providers.Web3Provider(window?.ethereum, "any")
+}
+
+const config = {
+    transferabilityForUnitsOwner: true,
+    distributionFromAnyAddress: false,
+};
+
+const [loading, setloading] = useState(false)
+
+export const createNewPol = async ({ address }: any) => {
+    setloading(true)
+    try {
+        const signer = await provider?.getSigner();
+        const contract = new ethers.Contract(Abi.address, Abi.abi, signer)
+
+        const tx = await contract.createPool(token, address, config);
+        const receipt = await tx.wait();
+        console.log(receipt)
+        return receipt.events.find((event: any) => event.event === "PoolCreated").args
+            .pool;
+            setloading(false)
+    } catch (error) {
+        setloading(false)
+        console.log(error)
+
+    }
+}
+
+export const handleUpdateMembers = async ({ memberAddress }: any) => {
+    try {
+        const signer = await provider?.getSigner();
+        const contract = new ethers.Contract(Abi.address, Abi.abi, signer)
+
+        const tx = await contract.updateMemberUnits(token, memberAddress, config);
+        const receipt = await tx.wait();
+        console.log(receipt)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const distributeToken = async ({ from, pool, requestedAmount, userData}: any) => {
+    try {
+        const signer = await provider?.getSigner();
+        const contract = new ethers.Contract(Abi.address, Abi.abi, signer)
+
+        const tx = await contract.distribute(token, from, pool, requestedAmount, userData);
+        const receipt = await tx.wait();
+        console.log(receipt)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const claimToken = async ({ pool, memberAddress, userData}: any) => {
+    try {
+        const signer = await provider?.getSigner();
+        const contract = new ethers.Contract(Abi.address, Abi.abi, signer)
+
+        const tx = await contract.claimAll(pool, memberAddress, userData);
+        const receipt = await tx.wait();
+        console.log(receipt)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
