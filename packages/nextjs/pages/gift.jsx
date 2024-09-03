6c995f37169f6740e354ed36f678d1efc608fa99
amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-09-03 18:08:16 +0100

    update code

--- packages/nextjs/pages/gift.jsx
+++ packages/nextjs/pages/gift.jsx
@@ -4,7 +4,7 @@ import GiftForm from "../components/GiftForm";
 const Gift = () => {
   return (
     <div className="  text-white ">
-      {/* <p>Gift</p> */}
+      
       <div className=" h-[600px] max-sm:h-[400px]  px-24 py-6">
         <div className=" flex justify-center items-center">
           <GiftForm />
